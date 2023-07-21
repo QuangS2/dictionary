375d1b09dfc0f46a5f0caf0d8085c6a65b107c3a
QuangS2 <2051052108Quang@ou.edu.vn>
2023-07-21 22:38:38 +0700

    buoi2

--- flask/nameApp/utils.py
+++ flask/nameApp/utils.py
@@ -1,2 +1,12 @@
-# Phuong thuc ho tro
-# tuong tac CSDL
+def cart_stats(cart):
+    total_amount, total_quantity = 0, 0
+
+    if cart:
+        for c in cart.values():
+            total_quantity += c['quantity']
+            total_amount += c['quantity'] * c['price']
+
+    return {
+        'total_amount': total_amount,
+        'total_quantity': total_quantity
+    }
